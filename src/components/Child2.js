285395099c95651390b32419cbffcaac56cd6286
Joseph Walsh <josephwalsh@arista.com>
2022-09-25 14:51:18 +0100

    exploration: added variable control to document editor

--- src/components/Child2.js
+++ src/components/Child2.js
@@ -1,10 +1,50 @@
-import "./htmlstyle.css";
+import { useState } from "react";
 
 function Child2(props) {
-  console.log(props.data);
+  console.log("props: " + props.data);
+  //const [newV, setNewV] = useState("");
+  const [visVars, setVisVars] = useState(null);
+  const [testy, setTesty] = useState("");
+
+  function addContent(e) {
+    e.preventDefault();
+    visVars ? setVisVars(false) : setVisVars(true);
+  }
+
+  function handleAdd(e) {
+    e.preventDefault();
+    setTesty(e.target.value);
+  }
+
+  const showHideVars = visVars ? (
+    <span>Hide Variables</span>
+  ) : (
+    <span>Show Variables</span>
+  );
+
+  /*
+  function addTest(e) {
+    e.preventDefault();
+    setNewV(textV);
+  }
+*/
+  var test = visVars ? (
+    <div>
+      {props.data.map(function (i, index) {
+        return (
+          <button onClick={handleAdd} value={i.content} key={index}>
+            {i.name}
+          </button>
+        );
+      })}
+    </div>
+  ) : null;
+
   return (
     <div>
-      <span dangerouslySetInnerHTML={{ __html: props.data }} />
+      <button onClick={addContent}>{showHideVars}</button>
+      {test}
+      <span dangerouslySetInnerHTML={{ __html: testy }} />
     </div>
   );
 }
--- src/components/textEditor.js
+++ src/components/textEditor.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Child2 from "./Child2";
+import _ from "lodash";
 
 import { Editor, EditorState, RichUtils } from "draft-js";
 import { convertToHTML } from "draft-convert";
@@ -9,11 +10,27 @@ import "./style.css";
 
 function TextEditor() {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
   const editor = React.useRef(null);
+  const [arr1, setArr1] = useState([]);
+  //test
+
+  console.log(arr1[0]);
+  console.log("data: " + data[0]);
 
   function focusEditor() {
     editor.current.focus();
+  }
+
+  function handleSaveName(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+
+  function handleSave() {
+    const save = convertToHTML(editorState.getCurrentContent());
+    setArr1((current) => [...current, { name: name, content: save }]);
   }
 
   function handleSubmit(e) {
@@ -28,7 +45,7 @@ function TextEditor() {
     // setData(editorState.getCurrentContent().getBlocksAsArray());
     const html = convertToHTML(editorState.getCurrentContent());
     console.log(html);
-    setData(html);
+    setData((current) => [...current, { name: name, content: html }]);
   }
 
   useEffect(() => {
@@ -42,6 +59,12 @@ function TextEditor() {
     };
     return <button onMouseDown={onClickButton}>{props.label}</button>;
   };
+
+  const diff = _.difference(arr1, data);
+
+  if (diff.length) {
+    return <p>Unsaved changes</p>;
+  }
 
   const BLOCK_TYPES = [
     { label: "H1", style: "header-one" },
@@ -105,11 +128,14 @@ function TextEditor() {
   return (
     <div className="main">
       <div className="one">
+        <h1>Editor</h1>
+        <input type="text" onChange={handleSaveName} value={name} />
         <div onClick={focusEditor}>
           <div className="App">
             <BlockStyleControls onToggle={onBlockClick} />
             <InlineStyleControls onToggle={onInlineClick} />
           </div>
+
           <div className="App">
             <Editor
               ref={editor}
@@ -118,9 +144,13 @@ function TextEditor() {
             />
           </div>
           <input type="submit" onClick={handleSubmit} />
+          <button onClick={handleSave}>Save</button>
         </div>
+        {diff.length ? <p>Unsaved changes</p> : null}
       </div>
+
       <div className="two">
+        <h1>Document Creator</h1>
         <Child2 data={data} />
       </div>
     </div>
