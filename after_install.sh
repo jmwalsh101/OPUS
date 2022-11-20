4bcd5cc37b60d882f484e9b448e5aa658ff9f050
Joseph Walsh <josephwalsh@arista.com>
2022-11-20 23:46:02 +0000

    trying to fix npm issue

--- after_install.sh
+++ after_install.sh
@@ -1,7 +1,8 @@
 cd /home/ubuntu/client
 npm install
 npm install --save react react-dom react-scripts react-particles-js
-cd .. && cd server
+cd .. 
+cd server
 npm install
 cd ..
 npm install pm2 -g
