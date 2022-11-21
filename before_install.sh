dcf66ab8a94fb5bee69e0cb87efc01cd13d71412
Joseph Walsh <josephwalsh@arista.com>
2022-11-21 00:14:23 +0000

    switched out to nvm

--- before_install.sh
+++ before_install.sh
@@ -1,3 +1,5 @@
 cd /home/ubuntu/
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-apt-get install --yes nodejs npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+nvm install v10.15.3
+nvm alias default v10.15.3
+
